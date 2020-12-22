7e5c3b0b0c29a5f32aa83a572b7df12a6f194614
Cristina Tuñí i Domínguez <c.tuni.dominguez@gmail.com>
2020-12-22 13:41:25 +0100

    Update anaconda_setup.py

--- Scripts/anaconda_setup.py
+++ Scripts/anaconda_setup.py
@@ -8,7 +8,7 @@ os.system('sha256sum Anaconda3-2020.07-Linux-x86_64.sh')
 os.system('bash Anaconda3-2020.07-Linux-x86_64.sh')
 #Compiling from source
 os.system('source ~/.bashrc')
-os.system('Anaconda3-2020.07-Linux-x86_64.sh')
+os.system('rm Anaconda3-2020.07-Linux-x86_64.sh')
 #Using conda to install bowtie2, samtools and bedtools
 os.system('conda install -c bioconda bowtie2')
 os.system('conda install -c bioconda samtools')
