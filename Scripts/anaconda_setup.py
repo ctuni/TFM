972c93407860e9397a2eec2bcbc211afde894a76
Cristina Tuñí i Domínguez <c.tuni.dominguez@gmail.com>
2021-01-25 14:57:01 +0100

    Update anaconda_setup.py
    Trying to make it run on Windows since it stopped working

--- Scripts/anaconda_setup.py
+++ Scripts/anaconda_setup.py
@@ -1,11 +1,11 @@
 import os
 
 #Download the file for Linux, altough this script will run only on Windows Subsystem for Linux
-os.system('curl -O https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh')
+os.system('wget https://repo.continuum.io/archive/Anaconda3-2020.11-Linux-x86_64.sh')
 #Checking the integrity of the file
-os.system('sha256sum Anaconda3-2020.07-Linux-x86_64.sh')
+os.system('sha256sum Anaconda3-2020.11-Linux-x86_64.sh')
 #Running the .sh script
-os.system('bash Anaconda3-2020.07-Linux-x86_64.sh')
+os.system('bash Anaconda3-2020.11-Linux-x86_64.sh')
 #Compiling from source
 os.system('source ~/.bashrc')
 os.system('rm Anaconda3-2020.07-Linux-x86_64.sh')
